6c0b4c387d8313aa2b6c49bcd508c3f1b096ee35
dromerodev <dromerosl@MacBook-Pro-de-Diego.local>
2021-03-25 23:56:40 -0300

    product detail

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { IProduct } from './product';
+import { Subscription } from 'rxjs';
+import { ProductService } from '../product.service';
+import { IProduct } from '../product';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss'],
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product Detail: ';
   product: IProduct | undefined;
+  errorMessage: string = '';
+  sub!: Subscription;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.sub = this.productService.getProducts().subscribe({
+      next: (res) => {
+        this.product = res.find((product) => {
+          return product.productId === id;
+        });
+      },
+      error: (err) => (this.errorMessage = err),
+    });
   }
 
   onBack(): void {
     this.router.navigate(['/products']);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
